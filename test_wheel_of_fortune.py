f4b161e2d6187021326e17a00aa4a5b316785996
jisep <jesua19uni@gmail.com>
2021-03-23 00:32:00 +0800

    Added test for 'guess_phrase' method

--- test_wheel_of_fortune.py
+++ test_wheel_of_fortune.py
@@ -3,47 +3,35 @@ from wheel_of_fortune import Phrase, Player
 
 class TestPhrase(unittest.TestCase):
 	def test_initial_value(self):
-		# Test with alphanumerical strings 
-		phrase_1 = Phrase(category="Film",phrase="Die Hard 2")
+		# Test with alphabetical strings 
+		phrase_1 = Phrase(category="Film",phrase="Die Hard")
 		self.assertEqual(phrase_1.category,"Film")
-		self.assertEqual(phrase_1.phrase,"Die Hard 2")
-		self.assertEqual(phrase_1.blanked ,"___ ____ _")
+		self.assertEqual(phrase_1.phrase,"Die Hard")
+		self.assertEqual(phrase_1.blanked ,"___ ____")
 		self.assertEqual(phrase_1.guessed,"")
 		self.assertEqual(phrase_1.has_vowels,True)
-		# Test with strings containing non-alphanumerical characters
+		# Test with strings containing non-alphabetical characters
 		phrase_2 = Phrase("Film","Spider-Man: Into the Spider-Verse")
 		self.assertEqual(phrase_2.phrase,"Spider-Man: Into the Spider-Verse")
 		self.assertEqual(phrase_2.blanked,"______-___: ____ ___ ______-_____")
 		self.assertEqual(phrase_2.guessed,"")
 		self.assertEqual(phrase_2.has_vowels,True)
-		# Test with strings containing only non-alphanbetical characters
-		phrase_3 = Phrase("Film","4.3.2.1.")
-		self.assertEqual(phrase_3.phrase,"4.3.2.1.")
-		self.assertEqual(phrase_3.blanked,"_._._._.")
-		self.assertEqual(phrase_3.guessed,"")
-		self.assertEqual(phrase_3.has_vowels,False)
-		# Test with empty strings
-		phrase_4 = Phrase("","")
-		self.assertEqual(phrase_4.category,"")
-		self.assertEqual(phrase_4.phrase,"")
-		self.assertEqual(phrase_4.blanked,"")
-		self.assertEqual(phrase_4.guessed,"")
-		self.assertEqual(phrase_4.has_vowels,False)
 
-	def test_ascii_values(self):
-		# Test ValueError is raised when string has non-ascii characters 
-		self.assertRaises(ValueError, Phrase, "Film","後來的我們") 
+	def test_numerical_values(self):
+		# Test ValueError is raised when string has numerical characters 
+		self.assertRaises(ValueError, Phrase, "Title","Home alone 2") 
+		self.assertRaises(ValueError, Phrase, "Title","4.3.2.1.")
+		# Test ValueError is raised when input string is empty
+		self.assertRaises(ValueError, Phrase, "","") 
 
 	def test_guess_letter(self):
-		# Test that function returns False if guessed letter is not in phrase
+		# Test that function returns tuple (False,0) if guessed letter is not in phrase
 		phrase_1 = Phrase("Song","Whitney Houston's I Will Always Love You")
-		self.assertEqual(phrase_1.guess_letter('x'),False)
-		# Test that instance variables are correctly updated after correctly guessing numerical character
-		phrase_2 = Phrase("Film","Mission Impossible 3")
-		phrase_2.guess_letter('3')
-		self.assertEqual(phrase_2.blanked,"_______ __________ 3")
-		self.assertEqual(phrase_2.guessed,'3')
-		self.assertEqual(phrase_2.has_vowels,True)
+		self.assertEqual(phrase_1.guess_letter('x'),(False,0))
+		# Test that function returns tuple (True,guessed_letters) if letter is correctly guessed 
+		phrase_2 = Phrase("Title","Dr. Quinn Medicine Woman")
+		self.assertEqual(phrase_2.guess_letter('n'),(True,4))
+	
 		# Test that instance variables are correctly updated after correctly guessing letter
 		phrase_3 = Phrase("Film","Pulp Fiction")
 		phrase_3.guess_letter('p')
@@ -59,6 +47,18 @@ class TestPhrase(unittest.TestCase):
 		self.assertEqual(phrase_4.blanked,"Ha_hi: A _o_'_ _a_e")
 		self.assertEqual(phrase_4.guessed,"pbahreio")
 		self.assertEqual(phrase_4.has_vowels,False)
+
+	def test_guess_phrase(self):
+		# Test that method returns True when phrase is correctly guessed
+		phrase_1 = Phrase("Headline","Madonna Gives Birth To A Baby Girl")
+		self.assertEqual(phrase_1.guess_phrase("Madonna Gives Birth To A Baby Girl"),True)
+		# Test that method returns None when phrase is not correctly guessed
+		phrase_2 = Phrase("Things","Espresso Cappuccino & Decaf Coffee")
+		self.assertEqual(phrase_2.guess_phrase("Expresso Cappuccino & Decaf Coffee"),None)
+		# Test that "blanked" variable is correctly updated if phrase is correctly guessed
+		phrase_3 = Phrase("Nickname","The Land Of Plenty")
+		phrase_3.guess_phrase("The Land Of Plenty")
+		self.assertEqual(phrase_3.blanked,"The Land Of Plenty")
 
 class TestPlayer(unittest.TestCase):
 	def test_initial_value(self):
@@ -89,19 +89,29 @@ class TestPlayer(unittest.TestCase):
 	def test_guessed_letter(self):
 		# Test "money" variable is correctly updated
 		player_1 = Player("Jay")
-		player_1.guessed_letter(amount=900,nb_letters=3)
+		player_1.guessed_letter(amount=900,guessed_letters=3)
 		self.assertEqual(player_1.money,2700)
 		self.assertEqual(player_1.prize,[])
 		# Test passing default value (False) to "prize" parameter has no additional effect
 		player_2 = Player("Mitchel")
-		player_2.guessed_letter(amount=400,nb_letters=2,prize=False)
+		player_2.guessed_letter(amount=400,guessed_letters=2,prize=False)
 		self.assertEqual(player_2.money,800)
 		self.assertEqual(player_2.prize,[])
 		# Test "prize" vaable is correctly updated
 		player_3 = Player("Phil")
 		player_3.money = 750
 		player_3.prize = ["A trip to Ann Arbor!"]
-		player_3.guessed_letter(amount=250,nb_letters=1,prize="A brand new car!")
+		player_3.guessed_letter(amount=250,guessed_letters=1,prize="A brand new car!")
 		self.assertEqual(player_3.money,1000)
 		self.assertEqual(player_3.prize,["A trip to Ann Arbor!","A brand new car!"])
+'''
+	def test_dunder_str(self):
+		player = Player("Computer")
+		player.money = 500
+		player.prize.append["A trip to Paris"]
+		self.assertEqual(player.__str__(), "Name: Computer \nMoney prize: 500 \nPrizes: [A trip to Paris]")
+'''
+
+	# REPAIR TEST DUNDER STR	
+
 		
